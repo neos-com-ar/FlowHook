56000749c0bf7b7b52f92164160bd8ece2d7a6d1
dmaltaneri@neos.com.ar <dmaltaneri@neos.com.ar>
2025-11-11 17:14:46 -0300

    Fix Refresh automatico

--- app/providers.jsx
+++ app/providers.jsx
@@ -3,6 +3,14 @@
 import { SessionProvider } from 'next-auth/react';
 
 export default function Providers({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider
+      refetchOnWindowFocus={false}
+      refetchOnMount={false}
+      refetchInterval={0}
+    >
+      {children}
+    </SessionProvider>
+  );
 }
 
